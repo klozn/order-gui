ea6e84905697e5903d7f0d32ba70243a2a6b5e13
Klaas Devits <klaas.devits@gmail.com>
2021-12-02 10:03:16 +0100

    basic layout implemented

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {ItemOverviewComponent} from "./item-overview/item-overview.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/items', pathMatch: 'full' },
+  { path: 'items', component: ItemOverviewComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
